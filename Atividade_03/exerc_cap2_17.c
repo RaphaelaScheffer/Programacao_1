b0bab28d303a10c65a00b799a27ec2b265066aaf
RaphaelaScheffer <106411932+RaphaelaScheffer@users.noreply.github.com>
2022-06-14 16:09:21 -0300

    Rename main.c to exerc_cap2_17.c

--- Atividade_03/exerc_cap2_17.c
+++ Atividade_03/exerc_cap2_17.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-/* Depois da liberação do governo para as mensalidades dos planos de saúde, as
-pessoas começaram a fazer pesquisas para descobrir um bom plano, não muito
-caro. Um vendedor de um plano de saúde apresentou a tabela a seguir. Faça um
+/* Depois da liberaÃ§Ã£o do governo para as mensalidades dos planos de saÃºde, as
+pessoas comeÃ§aram a fazer pesquisas para descobrir um bom plano, nÃ£o muito
+caro. Um vendedor de um plano de saÃºde apresentou a tabela a seguir. FaÃ§a um
 programa que entre com o nome e a idade de uma pessoa e imprima o nome e o
-valor que ela deverá pagar.
+valor que ela deverÃ¡ pagar.
                                    Idade               Valor
-                               Até 10 anos           R$30,00
-                        Acima de 10 até 29 anos      R$60,00
-                        Acima de 29 até 45 anos      R$120,00
-                        Acima de 45 até 59 anos      R$150,00
-                        Acima de 59 até 65 anos      R$250,00
+                               AtÃ© 10 anos           R$30,00
+                        Acima de 10 atÃ© 29 anos      R$60,00
+                        Acima de 29 atÃ© 45 anos      R$120,00
+                        Acima de 45 atÃ© 59 anos      R$150,00
+                        Acima de 59 atÃ© 65 anos      R$250,00
                            Maior que 65 anos         R$400,00 */
 
 
