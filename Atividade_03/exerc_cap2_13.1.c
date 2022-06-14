7a19fdb126f80fc91f33cdc91cc68f4d2f09b797
RaphaelaScheffer <106411932+RaphaelaScheffer@users.noreply.github.com>
2022-06-14 15:15:53 -0300

    Rename main.c to exerc_cap2_13.1.c

--- Atividade_03/exerc_cap2_13.1.c
+++ Atividade_03/exerc_cap2_13.1.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-/* Faça um programa que permita entrar com o nome, a nota da prova 1 e a nota da
+/* FaÃ§a um programa que permita entrar com o nome, a nota da prova 1 e a nota da
 prova 2 de um aluno. O programa deve imprimir o nome, a nota da prova 1, a nota
-da prova 2, a média das notas e uma das mensagens: "Aprovado", "Reprovado"ou
-"em Prova Final"(a média é 7 para aprovação, menor que 3 para reprovação e as
+da prova 2, a mÃ©dia das notas e uma das mensagens: "Aprovado", "Reprovado"ou
+"em Prova Final"(a mÃ©dia Ã© 7 para aprovaÃ§Ã£o, menor que 3 para reprovaÃ§Ã£o e as
 demais em prova final).*/
 
 // if
