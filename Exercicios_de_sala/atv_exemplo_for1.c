7b4fd0edb88a769d4adc06704f384b1d2be71740
RaphaelaScheffer <106411932+RaphaelaScheffer@users.noreply.github.com>
2022-06-23 19:29:45 -0300

    Add files via upload

---
+++ Exercicios_de_sala/atv_exemplo_for1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+   
+     int main () { 
+     
+      int i;  
+      printf("sao divisiveis por 3:\n");
+      
+      
+      for (i=3;i<=40;i+=3) {
+      printf("%d\n", i);}
+      }
+      
