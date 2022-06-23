c90ab3b2bede1a96191a6abc8e69a668565e2047
RaphaelaScheffer <106411932+RaphaelaScheffer@users.noreply.github.com>
2022-06-23 19:30:06 -0300

    Add files via upload

---
+++ Exercicios_de_sala/atv_exemplo_while.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+   
+     int main () { 
+     
+      int i;  
+      printf("os divisiveis por 3 e pares:\n");
+      
+      i=1;
+      while (i<=40){
+      
+      if (i % 3 == 0 && i % 2 == 0)
+      printf("%d\n", i);
+      i++;
+      }
+      }
+      
+      
