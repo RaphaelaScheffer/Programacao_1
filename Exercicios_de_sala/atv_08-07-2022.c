b5fd868cce1fb40b64781a3a7b2e94ab9e5aeecc
RaphaelaScheffer <106411932+RaphaelaScheffer@users.noreply.github.com>
2022-07-08 20:49:16 -0300

    Add files via upload

---
+++ Exercicios_de_sala/atv_08-07-2022.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    float nota[5];
+      int i;
+     for (i=0;i<5;i++) {
+      printf("Digite a nota do aluno %d: ", i);
+       scanf("%f", &nota[i]);
+    }
+         for (i=0;i<5;i++) {
+         printf(" Vetor[%d]: %.2f\n", i, nota[i]);
+         
+         }
+     return 0; 
+   }
