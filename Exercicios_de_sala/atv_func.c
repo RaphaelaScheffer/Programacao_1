51cc8e03bfc1a5258bc5b951a9c1a59c36f7b1cc
RaphaelaScheffer <106411932+RaphaelaScheffer@users.noreply.github.com>
2022-07-21 19:53:02 -0300

    Add files via upload

---
+++ Exercicios_de_sala/atv_func.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void qualquer_um(int num){
+   printf("Oi mundo! %d \n", num);
+   }
+int main(){
+
+  qualquer_um(6);
+  qualquer_um(100);
+  qualquer_um(4);
+  }
